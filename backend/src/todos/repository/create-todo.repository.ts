47175e877a4f693c2d48668770f82f333dc3781d
neves-rafa <rafaela.silva11@estudante.ifms.edu.br>
2026-03-06 12:30:10 -0400

    aula4

---
+++ backend/src/todos/repository/create-todo.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/databases/prisma.database";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+
+@Injectable()
+export class CreateTodoRepository{
+    constructor(private readonly prisma: PrismaService) {}
+
+    async execute(date: CreateTodoDto) {
+        return await this.prisma.todo.create({date});
+    }
+}
---
+++ backend/src/todos/repository/find-todo-by-id.repository.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/databases/prisma.database";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+
+@Injectable()
+export class CreateTodoRepository{
+    constructor(private readonly prisma: PrismaService) {}
+
+    async findAll() {
+        return await this.prisma.todo.findMany();
+    }
+}
+
