d5af7ea9ede1818dbff8020dab3095eeceb7d538
VitorSLima <dev.vitorlima@gmail.com>
2026-04-11 14:35:28 -0400

    Refactor Prisma schema and update DTOs for consistency; corrected variable names and improved code clarity in repository methods.

--- backend/src/todos/repository/update-todo.repository.ts
+++ backend/src/todos/repository/update-todo.repository.ts
@@ -1,15 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/shared/databases/prisma.database";
-import { UpdatetodoDto } from "../dto/update-todo.dto";
+import { UpdateTodoDto } from "../dto/update-todo.dto";
 @Injectable()
 export class UpdateTodoRepository{
     constructor(private readonly prisma: PrismaService) {}
 
-    async execute(date: UpdatetodoDto, id: string) {
+    async execute(data: UpdateTodoDto, id: string) {
         return await this.prisma.todo.update({
             where: {
             id
-        },date});
+        }, data});
     }
 }
 
